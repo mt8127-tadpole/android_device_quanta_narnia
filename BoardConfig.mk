02c902240758b1f1e33c47e977a65ab946ef1efa
Huck Jones <peanuthead_069@yahoo.com>
2019-11-28 18:50:24 -0500

    narnia: add prebuilt kernel and appropriate lines

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,18 @@
 # Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_narnia_defconfig
+#TARGET_KERNEL_CONFIG := cyanogenmod_narnia_defconfig
 BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
+
+BUILD_KERNEL_FROM_SOURCE := false
+ifeq ($(BUILD_KERNEL_FROM_SOURCE),true)
+    # build kernel from sources
+    BOARD_USES_MTK_KERNELBUILD := true    # from https://gitlab.com/SaberMod/slim-android-build/blob/d0ea96c4ec309e9361f8da6d12dc6770f04e57f4/core/mtk_utils.mk
+    TARGET_KERNEL_SOURCE := $(KERNEL_DIR)
+    TARGET_KERNEL_CONFIG := bitland8127_tb_l_defconfig
+    #TARGET_KMODULES := true         # is it needed ?
+else
+    # use prebuilt kernel
+    TARGET_PREBUILT_KERNEL := $(DEVICE_DIR)/prebuilt/kernel
+endif
 
 # inherit from common mt8127
 -include device/mediatek/mt8127-common/BoardConfigCommon.mk
